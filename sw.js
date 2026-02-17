89ba79eacc3a65b01c9acc33bce8ce53e0f8bdc0
Damon  Lu <59256766+WhatDamon@users.noreply.github.com>
2026-02-17 20:47:02 +0800

    A total refactor

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'pseudoloc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './src/app.js',
+  './src/processor.js',
+  './css/style.css',
+  './data/i18n.json',
+  './data/character.json',
+  './data/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          if (event.request.url.startsWith('http')) {
+            cache.put(event.request, fetchResponse.clone());
+          }
+          return fetchResponse;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
